1a8991a672b54ab59c19fa4f50db78bc7b4713fa
Sal Ahmed <sa1749@york.ac.uk>
2020-11-20 17:20:43 +0000

    v0.01

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
-const int width  = 800;
-const int height = 800;
+const int width  = 100;
+const int height = 100;
 
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor colour) { //Utilising Bresenhelm's line drawing algorithm
